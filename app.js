88405bdcc6281400bd6fe1a5b17cdffb4cb5226b
Shashibhushan Singh <shashibhushansingh11@gmail.com>
2026-06-17 10:49:22 +0530

    lec-23

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      name: "Shashibhushan",
-      age: 33,
-      ImageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTHW7C1B9Qgc_Twc7HlBKEjech0uFn7dkK7A&s",
+      counter: 0,
     };
-  },
-
-  methods: {
-    calculateAge() {
-      return this.age + 5;
-    },
   },
 });
 
-app.mount("#assignment");
+app.mount('#events');
