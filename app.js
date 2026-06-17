a133cae2a8f30ca246069243686070b77493b7d2
Shashibhushan Singh <shashibhushansingh11@gmail.com>
2026-06-17 11:38:29 +0530

    26

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
-    add() {
-      this.counter = this.counter + 1;
+    setName(event) {
+      this.name = event.target.value + " " + "singh";
     },
-    reduce() {
-      this.counter = this.counter - 1;
+
+    add(num) {
+      this.counter = this.counter + num;
+    },
+
+    reduce(num) {
+      this.counter = this.counter - num;
     },
   },
 });
